5fe332ae7b07fb53fe9b8625f7ac77842d822263
Michele Rawlins <shellbop@att.net>
2020-06-09 20:26:59 -0500

    single delete works

--- src/components/pages/SingleItem/SingleItem.js
+++ src/components/pages/SingleItem/SingleItem.js
@@ -11,14 +11,22 @@ class SingleItem extends React.Component {
   componentDidMount() {
     const { itemId } = this.props.match.params;
     itemData.getSingleItem(itemId)
-      .then((tesponse) => this.setState({ item: Response.data }))
-      .catch((err) => console.error('unable to ge tsingle item:', err));
+      .then((response) => this.setState({ item: response.data }))
+      .catch((err) => console.error('unable to get single item:', err));
+  }
+
+  removeItem = () => {
+    const { itemId } = this.props.match.params;
+    itemData.deleteItem(itemId)
+      .then(() => this.props.history.push('/home'))
+      .catch((err) => console.error('unable to delete single', err));
   }
 
   render() {
     const { item } = this.state;
     return (
       <div className="SingleItem">
+         <button className="btn btn-danger" onClick={this.removeItem}><i className="fas fa-trash-alt"></i></button>
         <h1>{item.itemName}</h1>
         <img src={item.itemImage} alt="item"/>
         <p>{item.itemDescription}</p>
